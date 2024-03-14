900aa02e74450cf94fe4c9c54c10153c0ddacfd2
SUVAM DAS <dsuvam954@gmail.com>
2024-03-14 21:04:24 +0530

    update

--- project2/script.js
+++ project2/script.js
@@ -1,27 +1,24 @@
 var tl=gsap.timeline()
-gsap.to("#yellow",{
+gsap.to("#yellow1",{
     top:"-100%",
 delay:0.5,
-duration:1,
+duration:0.7,
     ease: "expo.out"
 })
 
-tl.to("#yellow1",{
-    top:"-100%",
-    delay:0.5,
-    duration:0.5,
-        ease: "expo.out"
-
-})
 tl.from("#yellow2",{
     top:"-100%",
-    delay:0.5,
+    delay:0.9,
     duration:0.5,
         ease: "expo.out"
 
 },"anim")
 
 tl.to("#loader h1",{
-    color:"black"
-    delay:0.6
+    color:"black",
+    delay:0.7
 },"anim")
+
+tl.to("#loader",{
+    opacity:0
+})
