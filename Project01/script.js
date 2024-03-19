08388e1945bd79a989ff92b84f309a34e938580c
SUVAM DAS <dsuvam954@gmail.com>
2024-03-19 22:26:30 +0530

    update js

--- Project01/script.js
+++ Project01/script.js
@@ -40,38 +40,31 @@ function circleMousefollower(xscale,yscale){
 circleMousefollower();
 first();
 
+document.querySelectorAll(".elem").forEach(function (elem) {
+  var rotate = 0;
+  var diffrot = 0;
 
-document.querySelectorAll(".elem").forEach(function(elem){
-  var rotate=0;
-  var diff=0;
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
 
-  elem.addEventListener("mouseleave",function(dets){
-
-          gsap.to( elem.querySelector("img"),{
-           opacity:0,
-           ease:Power3,
-           duration:0.5,
-        
-          });
-       });
-
-
-  elem.addEventListener("mousemove",function(dets){
-
- var diff=dets.clientY-elem.getBoundingClientReact().top;
- diff=dets.clientX-rotate;
- rotate=dets.clientX;
-
-     gsap.to( elem.querySelector("img"),{
-      opacity:1,
-      ease:Power3,
-      top:diff,
-      left:dets.clientX,
-      rotate: gsap.utils.clamp(-20,20,diff*0.2),
-     });
+  elem.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
   });
 });
-
 // function showTime(){
 //     var date = new Date();
 //     var h = date.getHours(); // 0 - 23
